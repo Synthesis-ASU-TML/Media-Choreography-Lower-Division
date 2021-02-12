420aed1cd9fc03fdca38af57631081eb250de418
Connor Rawls <connor.rawls@gmail.com>
2021-02-11 21:37:47 -0700

    attempt at js painter files

---
+++ utility/button_painter.js
@@ -0,0 +1,9 @@
+function paint() {
+    var val = box.getvalueof()[0]
+    var viewsize = mgraphics.size
+    var valrange = box.getattr("size")
+    var width = viewsize[0]
+    var height = viewsize[1]
+    
+    
+}
